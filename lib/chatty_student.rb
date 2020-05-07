cc929d7329b28e04686084232cdeedb1168ca1c7
Bryce B <bryce.boddy@gmail.com>
2020-05-07 08:21:58 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -2,11 +2,15 @@ class ChattyStudent < Student
   def hello
     super
       puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
+<<<<<<< HEAD
   end
   
   def raise_hand
     super
     9.times {puts "Pick me!"}
+=======
+    super
+>>>>>>> b1c429f63a002ff77b1e198fa06be4540798dae3
   end
 end
     
